37dbe9bf4556dea8ab126ac97a94d1a5ece2196c
user@az10tr.onmicrosoft.com <user@az10tr.onmicrosoft.com>
2019-05-16 12:39:13 +0200

    add model

---
+++ TraningAzureWeb/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraningAzureWeb.Models
+{
+    public class UserModel
+    {
+        public int Name { get; set; }
+    }
+}
